b08c58ca7659c6fa0b49b75bc10c34dbab65a3df
carina-pereira-devops <devops79@yahoo.com>
2024-10-06 16:11:29 -0300

    Removendo AddOns EkS

--- iac_eks/Main.tf
+++ iac_eks/Main.tf
@@ -68,11 +68,11 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
 # Armazenamento de bloco (Necessidade futura?)
-  cluster_addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
-  }
+#  cluster_addons = {
+#    aws-ebs-csi-driver = {
+#      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#    }
+#  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -117,17 +117,17 @@ module "eks" {
 
 # Rever configurações do EBS posteriormente
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
+#data "aws_iam_policy" "ebs_csi_policy" {
+#  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#}
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
+#module "irsa-ebs-csi" {
+#  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#  version = "5.39.0"
+#
+#  create_role                   = true
+#  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+#  provider_url                  = module.eks.oidc_provider
+#  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+#  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#}
