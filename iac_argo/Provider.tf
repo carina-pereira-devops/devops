2751c6f8270b88cfecf2b536888adcb8c9de6330
carina-pereira-devops <devops79@yahoo.com>
2024-10-07 08:31:54 -0300

    Ajuste na configuração do Provider

--- iac_argo/Provider.tf
+++ iac_argo/Provider.tf
@@ -1,25 +1,9 @@
 
 terraform {
-
+  required_version = ">= 1.0"
   required_providers {
-    aws =  {
-      source  = "hashicorp/aws"
-      version = "~> 5.47.0"
-    } 
-
     kubectl = {
       source  = "alekc/kubectl"
       version = "2.0.4"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.5.1"
-    }
   }
-}
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
